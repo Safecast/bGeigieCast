a9c824e6ca616a3e495398430d783e4c6ec5229e
Rob Oudendijk <rob@yr-design.biz>
2019-02-17 13:58:51 +0900

    Added files

--- temp/Serial read.cpp
+++ temp/Serial read.cpp
@@ -1,21 +1,24 @@
-HardwareSerial Serial1(2);
+/* Reads the data from serial 2 (RX=17 TX=16) at 9600 and send to serial(0) at 115200 (RX, TX on board Wemos D1 mini32 V1.0.0)
+*/
 
-void setup() {
-  // initialize both serial ports:
-  Serial.begin(115200);
-  Serial1.begin(115200);
+#include <Arduino.h>
+
+void setup()
+{
+    // initialize both serial ports:
+    Serial.begin(115200);
+    Serial2.begin(9600);
+    Serial.print("Init done");
 }
 
-void loop() {
-  // read from port 1, send to port 0:
-  if (Serial1.available()) {
-    int inByte = Serial1.read();
-    Serial.write(inByte);
-  }
+void loop()
+{
+    // read from port 1, send to port 0:
+    if (Serial2.available())
+    {
+        int inByte = Serial2.read();
+        Serial.write(inByte);
 
-  // read from port 0, send to port 1:
-  if (Serial.available()) {
-    int inByte = Serial.read();
-    Serial1.write(inByte);
-  }
+    }
+
 }
