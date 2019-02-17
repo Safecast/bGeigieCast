9acb97b174fd46b2dc48f977f1c0022a064eb875
Rob Oudendijk <rob@yr-design.biz>
2019-02-17 13:57:17 +0900

    Serial read/send test

---
+++ temp/Serial read.cpp
@@ -0,0 +1,21 @@
+HardwareSerial Serial1(2);
+
+void setup() {
+  // initialize both serial ports:
+  Serial.begin(115200);
+  Serial1.begin(115200);
+}
+
+void loop() {
+  // read from port 1, send to port 0:
+  if (Serial1.available()) {
+    int inByte = Serial1.read();
+    Serial.write(inByte);
+  }
+
+  // read from port 0, send to port 1:
+  if (Serial.available()) {
+    int inByte = Serial.read();
+    Serial1.write(inByte);
+  }
+}
